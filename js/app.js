3d1ea229735eb83aa160ba3bcdff4659d62bdfac
Ralph Samaniego <rapsamaniego27@gmail.com>
2020-09-21 07:57:40 +0800

    added mobile event and changed arrow

--- js/app.js
+++ js/app.js
@@ -12,8 +12,22 @@ const profileSlider = new East({
 });
 
 
-const jovinaEl = document.querySelector('#jovina');
+function isMobile(){
+  if (navigator.userAgent.match(/Android/i) ||
+    navigator.userAgent.match(/webOS/i) ||
+    navigator.userAgent.match(/iPhone/i) ||
+    navigator.userAgent.match(/iPad/i) ||
+    navigator.userAgent.match(/iPod/i) ||
+    navigator.userAgent.match(/BlackBerry/i) ||
+    navigator.userAgent.match(/Windows Phone/i))
 
-jovinaEl.addEventListener('click', ()=> {
-  alert('Good Day Jovina! Hope you have a nice day today! Kain and workout din pag may time! :D');
+    return true;
+}
+
+const jovinaEl = document.querySelector('#jovina');
+const jovinaAction = isMobile() ? 'touchstart' : 'click';
+
+jovinaEl.addEventListener(jovinaAction, ()=> {
+  alert('Good Day Jovina! Hope you have a nice day today! Kain and workout din pag may time! :D Stay awesome!');
 });
+
